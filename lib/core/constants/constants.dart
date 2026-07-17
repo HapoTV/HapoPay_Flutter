4b16a65484433c66e0fc9dbbb79f659c92e2d124
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-07-17 23:28:30 +0300

    App Constants definition

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//Column Spacing Constants
+const verticalSpaceTiny = SizedBox(height: 5.0);
+const verticalSpaceSmall = SizedBox(height: 10.0);
+const verticalSpaceMedium = SizedBox(height: 20.0);
+const verticalSpaceLarge = SizedBox(height: 30.0);
+
+//Row Spacing Constants
+const horizontalSpaceSmall = SizedBox(width: 10.0);
+const horizontalSpaceMedium = SizedBox(width: 20.0);
+const horizontalSpaceLarge = SizedBox(width: 30.0);
