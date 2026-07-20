d9727de779e7156c526737e93dbf189547687d0e
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-07-20 18:02:55 +0300

    Create reusable AppPrimaryButton widget

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//Column spacing
+const verticalSpaceTiny = SizedBox(height: 5);
+const verticalSpaceSmall = SizedBox(height: 10);
+const verticalSpaceMedium = SizedBox(height: 15);
+const verticalSpaceLarge = SizedBox(height: 20);
+
+//Row spacing
+const horizontalSpaceTiny = SizedBox(width: 5);
+const horizontalSpaceSmall = SizedBox(width: 10);
+const horizontalSpaceMedium = SizedBox(width: 15);
+const horizontalSpaceLarge = SizedBox(width: 20);
